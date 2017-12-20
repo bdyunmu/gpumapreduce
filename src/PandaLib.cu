dd7c9e6d247cfca3ef16b12b2ecf182adbbcc00c
lihui <lihui@indiana.edu>
2017-12-19 22:06:16 -0500

    panda lib version

--- src/PandaLib.cu
+++ src/PandaLib.cu
@@ -9,8 +9,9 @@
 	First Version:		2012-07-01 V0.1
 	Current Version:	2012-09-01 V0.3
 	Last Updates:		2013-06-27 V0.44
-	
-	Developer: Hui Li (lihui@indiana.edu)
+	Last UPdates: 		2017-12-20 v0.6	
+	Developer: Hui Li (lihui@indiana.edu)  
+			  (huili@ruijie.com.cn)
 	This is the source code for Panda, a MapReduce runtime on GPUs and CPUs.
 	
  */	
