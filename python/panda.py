915c14fa2548f9989c0e887fd26686a2cb8a7fb7
lihui <li7hui@gmail.com>
2024-04-17 21:28:04 +0800

    add python panda.py interface

---
+++ python/panda.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+read the parameters, compiles, executes the Panda Apps like heapsort,terasort,topK.
+date: 2024/04/17
+author: huili@ruijie.com.cn
+"""
+import time
+
+print(time.asctime())
+print("Hello Panda!")
+print("Usage:")
+print("\tpython panda.py -c Config -l log -p parameters")
